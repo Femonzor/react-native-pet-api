db3c9e3cf7f841ef2a514d1fe31d0d94d033b86d
Femonzor <wayneyang3@gmail.com>
2019-02-14 23:53:22 +0800

    change folder

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -8,13 +8,13 @@ const signature = async (ctx: Koa.Context, next: Function) => {
   let folder: string;
   let tags: string;
   if (type === 'avatar') {
-    folder = 'avatar';
+    folder = 'react-native-pet/avatar';
     tags = 'app,avatar';
   } else if (type === 'video') {
-    folder = 'video';
+    folder = 'react-native-pet/video';
     tags = 'app,video';
   } else if (type === 'audio') {
-    folder = 'audio';
+    folder = 'react-native-pet/audio';
     tags = 'app,audio';
   }
   const sign = sha1(`folder=${folder}&tags=${tags}&timestamp=${timestamp}${config.cloudinary.api_secret}`);
